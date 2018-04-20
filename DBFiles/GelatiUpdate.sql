0de3c468f529dcaa3e1ee8ab25f84c88fb88832f
orrockjs2 <orrockjs2@vcu.edu>
2018-04-20 19:56:35 -0400

    added data to tables

---
+++ DBFiles/GelatiUpdate.sql
@@ -0,0 +1,24 @@
+-- Scotty Orrock
+-- GelatiUpdate.sql populates tables created from GelatiInit.sql
+
+
+
+INSERT INTO Employees
+VALUES(1, 
+(4, 'Steve Rosser', '4959 Eden Drive, Richmond VA, 23219', '804-858-4296'), (5, 'Kim Rosser', '4959 Eden Drive, Richmond VA, 23219', '804-858-4297'), (6, 'Keith Austin', '4371 Biddie Lane, Richmond VA, 23222', '804-971-3256'),
+(7, 'Erlich Bachman', '4089 Hickory Ridge Drive, Las Vegas NV, 89109', '702-698-8814'), (8, 'Richard Hendriks', '1341 Mesa Drive, Las Vegas NV, 89119', '702-361-0013'), (9, 'Ron Swanson','2910 Abia Martin Drive, New York NY, 10011', '646-727-3227'),
+(10, 'Leslie Knope', '3304 Redbund Drive, New York NY, 10013', '347-689-8441'), (11, 'Andy Dwyer', '4477 Bicetown Road, New York NY, 10007', '917-476-5197'),
+(12, 'April Ludgate', '3247 Gnatty Creek Road, New York NY, 10014', '516-303-5204'), (13, 'Ben Wyatt','4125 Duncan Avenue, New York NY, 10016', '917-594-7929'),
+(14, 'Ann Perkins', '387 Elm Drive, New York NY, 10004', '646-502-8307'), (15, 'Tom Haverford', '3030 Pallet Street, New York NY, 10001', '914-368-2716'), (16, 'Jerry Gergich', '3570 Oakwood Avenue, New York NY, 10021', '212-650-4851'),
+(17, 'Rick Grimes', '1876 North Street, Charlottesville VA, 22903', '434-963-4767'), (18, 'Carl Grimes', '1876 North Street, Charlottesville VA, 22903', '434-956-7955'), (19, 'Glen Rhee', '1350 Worley Avenue, Charlottesville VA, 22903', '434-293-1567'),
+(20, 'Maggie Green', '1361 South Street, Charlottesville VA, 22903', '434-245-0310'), (21, 'Daryl Dixon', '2344 North Street, Charlottesville VA, 22903', '434-970-7758'), (22, 'Morgan Jones', '1306 North Street, Charlottesville VA, 22903', '434-760-6898'),
+(23, 'Jesus Monroe', '', ''), (24, 'Eugene Porter', '', ''), (25, 'Carol Peletier', '', ''), (26, 'Negan', '', ''),
+(27, 'McLovin', '', ''),(28, 'Fogell', '', ''), (29, 'Evan','',''),
+(30, 'Seth', '', ''), (31, 'Tony Stark','',''), (32, 'Hulk', '', ''), 
+(33, 'Captain America', '', ''), (34, 'Black Widow', '', ''), (35, 'Spider Man', '', ''),
+(36, 'Black Panther', '', ''), (37, 'Doctor Strange', '', ''), (38, 'Thor', '', ''),
+(39, 'Vision', '', ''), (40, 'Sam Wilson', '', ''), (41, 'Ant Man', '', ''),
+(42, 'Star Lord', '', ''), (43, 'Drak the Destroyer', '', ''), (44, 'Groot', '', ''),
+(45, 'Gamora', '', ''), (46, 'Rocket Raccoon', '', ''), (47, 'Luke Skywalker', '', ''),
+(48, 'Darth Vader', '', ''), (49, 'Kylo Ren', '', ''), (50, 'Rey', '', '')
+
